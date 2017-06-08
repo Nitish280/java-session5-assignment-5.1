7bf451fd277f6f9cac146917b9234ac82b9928e3
Nitish280 <nsrccs@gmail.com>
2017-06-08 09:33:59 +0530

    Array

---
+++ src/Array.java
@@ -0,0 +1,20 @@
+
+public class Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      int[] Array = new int[10];
+      
+      int limit=10;
+      
+      System.out.println("Printing Even numbers between 1 and " + limit);
+      
+      for (int i=1; i<limit; i++) {
+    	  if( i % 2 == 0){
+              System.out.print(i + " ");
+    		  
+	}
+
+}
+}
+}
