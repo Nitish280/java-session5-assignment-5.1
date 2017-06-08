7ee262067669c0e7e8d661884357f76e63de382a
Nitish280 <nsrccs@gmail.com>
2017-06-08 09:44:02 +0530

    Create Array.java

--- src/Array.java
+++ src/Array.java
@@ -4,12 +4,13 @@ public class Array {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
       int[] Array = new int[10];
-      
+      //define
       int limit=10;
-      
+      //here we print even number between 1 to 10
       System.out.println("Printing Even numbers between 1 and " + limit);
       
       for (int i=1; i<limit; i++) {
+	      //if the number is perfectly divisible by 2 
     	  if( i % 2 == 0){
               System.out.print(i + " ");
     		  
